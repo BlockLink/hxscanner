b01aae1a34f078341a44ec181c5d662b217d7fc0
zoowii <zoowii@outlook.com>
2019-07-18 15:52:18 +0800

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+sudo docker image pull postgres:latest 
+sudo docker image pull adminer:latest
+
 go get github.com/bmizerany/pq
 go get github.com/pkg/errors
 go get golang.org/x/net/websocket
